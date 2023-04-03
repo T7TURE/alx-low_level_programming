f3bedb6bfb026caeb228eba1a6f98fcefa7252f2
T7TURE <ajnarak23@gmail.com>
2023-04-03 21:09:05 +0000

    Needle in a haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strstr - Locates a substring.
@@ -12,16 +11,28 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        size_t haystack_len = strlen(haystack);
-        size_t needle_len = strlen(needle);
-        size_t i;
-
-        for (i = 0; i <= haystack_len - needle_len; i++)
+        if (*needle == '\0')
         {
-                if (strncmp(&haystack[i], needle, needle_len) == 0)
+                return (haystack);
+        }
+
+        while (*haystack != '\0')
+        {
+                char *h = haystack;
+                char *n = needle;
+
+                while (*n != '\0' && *h == *n)
                 {
-                        return (&haystack[i]);
+                        h++;
+                        n++;
                 }
+
+                if (*n == '\0')
+                {
+                        return (haystack);
+                }
+
+                haystack++;
         }
 
         return (NULL);
