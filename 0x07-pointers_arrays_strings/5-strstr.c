59a11fe7bdcad2efccb28b095b74fd64473848f8
T7TURE <ajnarak23@gmail.com>
2023-04-04 03:44:42 +0000

    Needle in a haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,30 +11,29 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        if (*needle == '\0')
-        {
-                return (haystack);
-        }
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 
-        while (*haystack != '\0')
-        {
-                char *h = haystack;
-                char *n = needle;
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
 
-                while (*n != '\0' && *h == *n)
-                {
-                        h++;
-                        n++;
-                }
+		while (*n != '\0' && *h == *n)
+		{ 
+			h++;
+			n++;
+		}
+	
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
 
-                if (*n == '\0')
-                {
-                        return (haystack);
-                }
-
-                haystack++;
-        }
-
-        return (NULL);
+	return (NULL);
 }
 
