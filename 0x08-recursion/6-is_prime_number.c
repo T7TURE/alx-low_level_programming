876f752d7487ecda42becbf26d8733c3b2143288
T7TURE <ajnarak23@gmail.com>
2023-04-05 23:03:34 +0000

    prime numbers function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int prime_checker(int n, int i);
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: the number to check
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	return (0);
+}
+
+/**
+ * prime_checker - checks if a number is prime
+ * @n: the number to check
+ * @i: the current divisor to try
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int prime_checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (prime_checker(n, i - 1));
+}
+
