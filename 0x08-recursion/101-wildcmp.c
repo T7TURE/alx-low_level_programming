fb574056fa61c185e5abd6076fbde5d3d515e6bf
T7TURE <ajnarak23@gmail.com>
2023-04-06 06:41:35 +0000

    a function that compares two strings and returns 1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,28 @@
 #include "main.h"
-
-
 /**
- * wildcmp - compares two strings with wildcard support
- * @s1: first string to compare
- * @s2: second string to compare, with wildcards
- *
- * Description: This function compares two strings s1 and s2. The second
- * string may contain the wildcard character '*', which can match any
- * sequence of characters (including an empty sequence). Returns 1 if the
- * strings can be considered identical, 0 otherwise.
- *
- * Return: 1 if the strings can be considered identical, 0 otherwise.
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return 1;
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
-    
-	if (*s1 == *s2 || (*s2 == '*' && *s1 != '\0'))
-		return wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1);
-
-    
 	if (*s2 == '*')
-			return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
-
-     return (0);
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
-
