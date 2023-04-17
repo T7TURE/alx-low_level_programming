4487bcd5813c2a06ea7d879dbee8ed46f8570417
T7TURE <ajnarak23@gmail.com>
2023-04-17 20:17:35 +0000

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,65 +1,81 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
  *
- * Return: a new dog
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: new dog's owner
+ * Return: NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *new_name, *new_owner;
-	int name_len, owner_len;
+	dog_t *puppy;
 
-	/* Allocate memory for new dog */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	puppy = malloc(sizeof(dog_t));
+	if (puppy == NULL)
+		return (NULL);
+
+	puppy->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (puppy->name == NULL)
 	{
-		printf("Error: Failed to allocate memory for new dog\n");
-		exit(EXIT_FAILURE);
+		free(puppy);
+		return (NULL);
 	}
 
-	/* Allocate memory for new name */
-	name_len = 0;
-	while (name[name_len] != '\0')
-		name_len++;
-	new_name = malloc((name_len + 1) * sizeof(char));
-	if (new_name == NULL)
+	puppy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (puppy->owner == NULL)
 	{
-		printf("Error: Failed to allocate memory for new name\n");
-		free(new_dog);
-		exit(EXIT_FAILURE);
+		free(puppy->name);
+		free(puppy);
+		return (NULL);
 	}
 
-	/* Allocate memory for new owner */
-	owner_len = 0;
-	while (owner[owner_len] != '\0')
-		owner_len++;
-	new_owner = malloc((owner_len + 1) * sizeof(char));
-	if (new_owner == NULL)
-	{
-		printf("Error: Failed to allocate memory for new owner\n");
-		free(new_name);
-		free(new_dog);
-		exit(EXIT_FAILURE);
-	}
+	puppy->name = _strcopy(puppy->name, name);
+	puppy->age = age;
+	puppy->owner = _strcopy(puppy->owner, owner);
 
-	/* Copy name and owner to new strings */
-	for (int i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
-	for (int i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
-
-	/* Assign new strings to new dog */
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
-
-	return (new_dog);
+	return (puppy);
 }
-
